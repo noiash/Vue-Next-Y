e63e740668ef8838f14b62591c7111b97d12460b
noiash <noinoi51267@gmail.com>
2022-09-02 18:23:48 +0900

    [Add]Vue3 コンポーネント　共通パーツ

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import AppHeader from './components/AppHeader.vue'
 import { ref } from 'vue'
 
 const todos = ref([])
@@ -15,6 +16,7 @@ const removeTodo = (index) => {
 </script>
 
 <template>
+<AppHeader color="blue">My ToDo</AppHeader>
   <input type="text" size="30" v-model="newTodo">
   <button @click="addTodo()">追加</button>
 
